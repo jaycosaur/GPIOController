d3c09fbde833ac66c28ad61892699c6e838db8bf
Jacob Richter <>
2018-09-06 11:10:53 +1000

    Updated Server Funcitons

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.FAR_TRIGGER_PIN = 22
 exports.CLOSE_TRIGGER_PIN = 22
 exports.TRUCK_TRIGGER_PIN = 22
 exports.PIN_HIGH_DURATION = "50m"
-exports.SERVER_PORT=80
+exports.SERVER_PORT=8080
 exports.HOST="localhost"
