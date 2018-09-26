25e2a08f28be2d67da88b767432ab78dae87fac0
jaycosaur <jaycorichter@gmail.com>
2018-09-27 09:44:09 +1000

    Added additional triggering routes, basic website for testing at root path, added in flash routes

--- server.js
+++ server.js
@@ -4,15 +4,22 @@ const chalk = require('chalk');
 var config = require('./config')
 var CameraTrigger = require('./CameraTrigger')
 var gpsd = require('node-gpsd')
-
+var path = require('path');
 
 const farTrigger = new CameraTrigger({pin: config.FAR_TRIGGER_PIN, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
 const closeTrigger = new CameraTrigger({pin: config.CLOSE_TRIGGER_PIN, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
 const truckTrigger = new CameraTrigger({pin: config.TRUCK_TRIGGER_PIN, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
+const farTriggerFlash = new CameraTrigger({pin: config.FAR_TRIGGER_PIN_WITH_FLASH, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
+const closeTriggerFlash = new CameraTrigger({pin: config.CLOSE_TRIGGER_PIN_WITH_FLASH, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
+const truckTriggerFlash = new CameraTrigger({pin: config.TRUCK_TRIGGER_PIN_WITH_FLASH, pinHighDuration: config.PIN_HIGH_DURATION, inverse: false})
 
 console.log("FAR: ",  config.FAR_TRIGGER_PIN)
 console.log("CLOSE: ",  config.CLOSE_TRIGGER_PIN)
 console.log("TRUCK: ",  config.TRUCK_TRIGGER_PIN)
+
+console.log("FAR FLASH: ",  config.FAR_TRIGGER_PIN_WITH_FLASH)
+console.log("CLOSE FLASH: ",  config.CLOSE_TRIGGER_PIN_WITH_FLASH)
+console.log("TRUCK FLASH: ",  config.TRUCK_TRIGGER_PIN_WITH_FLASH)
 
 var app = express()
 
@@ -44,6 +51,10 @@ listener.connect(function() {
     })
 });
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.get('/trigger-far', async function (req, res) {
     await farTrigger.trigger(()=>res.send("OK"))
 })
@@ -56,10 +67,29 @@ app.get('/trigger-truck', async function (req, res) {
     await truckTrigger.trigger(()=>res.send("OK"))
 })
 
+app.get('/trigger-far-flash', async function (req, res) {
+    await farTriggerFlash.trigger(()=>res.send("OK"))
+})
+
+app.get('/trigger-close-flash', async function (req, res) {
+    await closeTriggerFlash.trigger(()=>res.send("OK"))
+})
+
+app.get('/trigger-truck-flash', async function (req, res) {
+    await truckTriggerFlash.trigger(()=>res.send("OK"))
+})
+
 app.get('/trigger-all', async function (req, res) {
     await farTrigger.trigger(()=>null)
     await closeTrigger.trigger(()=>null)
     await truckTrigger.trigger(()=>null)
+    res.send('done')
+})
+
+app.get('/trigger-all-flash', async function (req, res) {
+    await farTriggerFlash.trigger(()=>null)
+    await closeTriggerFlash.trigger(()=>null)
+    await truckTriggerFlash.trigger(()=>null)
     res.send('done')
 })
 
